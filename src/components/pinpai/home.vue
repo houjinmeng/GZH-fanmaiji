0f0301ec985b9734e856973b8cffe351e76e8653
hjm <841868101@qq.com>
2020-05-29 14:53:48 +0800

    last

--- src/components/pinpai/home.vue
+++ src/components/pinpai/home.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <div class="seven_echarts" id="seven"></div>
+    <div>jdklsjkldjk</div>
+    <div class="seven_echarts" id="seven" @click="charts"></div>
+    <div>jskjk</div>
   </div>
 </template>
 
 <script>
 export default {
   mounted() {
+    this.seven_option.xAxis.data.push(this.a);
+    this.seven_option.series[0].data.push(this.b);
     this.get_echarts();
   },
   data() {
     return {
+      a: "5/26",
+      b: "400",
       //   折线图
       seven_option: {
         title: {
@@ -19,9 +25,16 @@ export default {
         tooltip: {
           trigger: "axis"
         },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          top: "20%",
+          containLabel: true
+        },
         xAxis: {
           type: "category",
-          //boundaryGap: false,
+          boundaryGap: false,
           data: ["5/19", "5/20", "5/21", "5/22", "5/23", "5/24", "5/25"]
         },
         yAxis: {
@@ -37,8 +50,13 @@ export default {
             },
             smooth: true,
             data: [2, 10, 12, 21, 54, 260, 500],
-            lineStyle: {
-              color: "#60C0DD"
+            itemStyle: {
+              normal: {
+                color: "#8598d5",
+                lineStyle: {
+                  color: "#8598d5"
+                }
+              }
             }
           }
         ]
@@ -46,10 +64,15 @@ export default {
     };
   },
   methods: {
+    // 图表初始化
     get_echarts() {
       this.seven_chart = this.echarts.init(document.getElementById("seven"));
       // 把配置和数据放这里
       this.seven_chart.setOption(this.seven_option);
+    },
+    // 点击图表
+    charts() {
+      console.log(1);
     }
   }
 };
@@ -57,7 +80,8 @@ export default {
 <style scoped lang="less">
 // 折线图
 .seven_echarts {
-  height: 4rem;
+  height: 3.5rem;
   width: 100%;
+  background-color: #fff;
 }
 </style>
